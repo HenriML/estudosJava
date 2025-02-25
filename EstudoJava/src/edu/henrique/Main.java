3c2bdf1d18b5b50d49351a54783cd9891371bb74
Henrique Munhoz Leite <88989379+HenriML@users.noreply.github.com>
2025-02-25 16:25:41 -0300

    Fix: Project is now merged and working

---
+++ EstudoJava/src/edu/henrique/Main.java
@@ -0,0 +1,21 @@
+package edu.henrique;
+
+import edu.henrique.cursoNelioPrimeiroModulo.Operacoes;
+import edu.henrique.cursoNelioPrimeiroModulo.ProdutoInfo;
+
+public class Main {
+    public static void main(String[] args) {
+        //ProdutoInfo.executar();
+        System.out.println("Funfou:");
+        //Operacoes.somando();
+        //Operacoes.exemplo1();
+        //Operacoes.exemplo2();
+        //Operacoes.exemplo3();
+        //Operacoes.exemplo4();
+        //Operacoes.exemplo5();
+        ProdutoInfo.executar();
+    }
+}
+
+// Aula mais recente: #A021
+
